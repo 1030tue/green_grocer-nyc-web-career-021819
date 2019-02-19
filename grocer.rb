7cd7fcdb9b086f30c559d0e3f350ef822fd694c7
Kate <katekim5809@gmail.com>
2019-02-19 14:42:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,26 @@
 def consolidate_cart(cart)
   # code here
+  newhash={}
+  count = 0
+  cart.each do |grocery|
+    grocery.each do |item, val|
+      if newhash.has_key? (item)
+        newhash[item][:count]+=1
+      else
+        newhash[item]=val
+        newhash[item][:count] = 1
+      end
+    end
+  end
+  newhash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  newhash = {}
+  cart.each do |item, val|
+    coupons.each do |coupon|
+      if item == coupon[:item] && val[:count] >
 end
 
 def apply_clearance(cart)
